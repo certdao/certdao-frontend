0b5bdb4c3bb269b56144dcf4d362fc221f601eb6
0xbeary <0xbeary@protonmail.com>
2022-10-24 18:56:41 -0400

    Add tailwind template with daisyui

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex flex-col h-full items-center justify-center">
+      <div className="flex items-center">
+        <h1 className="text-6xl font-thin tracking-wider">Test</h1>
+      </div>
+      <p className="my-6 tracking-wide"></p>
+      <div className="mt-6 flex justify-center"></div>
+    </div>
+  );
+}
+
+export default App;
